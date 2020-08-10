30ccc23bdf01084c183ce3bb9e36274d48848328
ke <liuke311@126.com>
2020-08-10 10:37:19 +0800

    构建项目.

---
+++ ecds-vouchar/src/main/java/com/bosssoft/ecds/controller/VoucherController.java
@@ -0,0 +1,21 @@
+package com.bosssoft.ecds.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author liuke
+ * @since 2020-08-10
+ */
+@RestController
+@RequestMapping("/voucher-po")
+public class VoucherController {
+
+}
+
---
+++ ecds-vouchar/src/main/java/com/bosssoft/ecds/dao/VoucherDao.java
@@ -0,0 +1,16 @@
+package com.bosssoft.ecds.dao;
+
+import com.bosssoft.ecds.entity.po.VoucherPO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author liuke
+ * @since 2020-08-10
+ */
+public interface VoucherDao extends BaseMapper<VoucherPO> {
+
+}
