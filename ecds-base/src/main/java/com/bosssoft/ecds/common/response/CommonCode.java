a5c2751f7583ba96048a629026796e1a012e8b40
lsir <15188366128@163.com>
2020-08-11 10:14:20 +0800

    为CommonCode添加动态创建枚举方法，并添加枚举工具类

--- ecds-base/src/main/java/com/bosssoft/ecds/common/response/CommonCode.java
+++ ecds-base/src/main/java/com/bosssoft/ecds/common/response/CommonCode.java
@@ -1,5 +1,7 @@
 package com.bosssoft.ecds.common.response;
 
+import com.bosssoft.ecds.utils.DynamicEnumUtil;
+
 /**
  * @author: lpb
  * @create: 2020-07-10 09:02
@@ -28,6 +30,20 @@ public enum CommonCode implements ResultCode {
         this.code = code;
         this.message = message;
     }
+
+    /**
+     * 动态创建枚举类方法
+     * @param enumName 枚举名称
+     * @param success 是否成功
+     * @param code 枚举编码
+     * @param message 枚举消息
+     * @return
+     */
+    public static CommonCode addEnum(String enumName, boolean success, int code, String message){
+        CommonCode createCode = DynamicEnumUtil.addEnum(CommonCode.class, enumName, new Class[]{Boolean.class, Integer.class,String.class}, new Object[]{success,code, message});
+        return createCode;
+    }
+
     @Override
     public boolean success() {
         return success;
