b5e5fb7b73e148327622011b55d4110c9591cd8e
weixiao9999 <825030323@qq.com>
2020-08-12 11:20:35 +0800

    建立基本框架，增加依赖，增加配置文件

---
+++ ecds-bill-write-off/src/main/java/com/bosssoft/ecds/service/imp/UnitWriteOffServiceImpl.java
@@ -0,0 +1,12 @@
+package com.bosssoft.ecds.service.imp;
+
+import com.bosssoft.ecds.service.UnitWriteOffService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author hujierong
+ * @date 2020-8-12
+ */
+@Service
+public class UnitWriteOffServiceImpl implements UnitWriteOffService {
+}
