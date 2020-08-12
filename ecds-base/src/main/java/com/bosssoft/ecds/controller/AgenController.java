f4caa2efcf1e8f5a472e2c97b6d36971374e1704
vihenne-git <1109702845@qq.com>
2020-08-12 09:21:33 +0800

    添加了api注解

--- ecds-base/src/main/java/com/bosssoft/ecds/controller/AgenController.java
+++ ecds-base/src/main/java/com/bosssoft/ecds/controller/AgenController.java
@@ -8,6 +8,8 @@ import com.bosssoft.ecds.entity.vo.PageVO;
 import com.bosssoft.ecds.service.AgenService;
 import com.bosssoft.ecds.utils.MyBeanUtil;
 import com.bosssoft.ecds.utils.ResponseUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +26,7 @@ import java.util.List;
  */
 @Slf4j
 @RestController
+@Api(value = "单位管理接口")
 @RequestMapping("/agen")
 public class AgenController {
 
@@ -39,6 +42,7 @@ public class AgenController {
      * @author: YuHangChen
      * @time: 09/08/2020 上午10:17
      */
+    @ApiOperation(value = "新增单位")
     @PostMapping("/save")
     public String save(@RequestBody AgenVO fabAgenVO){
         AgenDTO fabAgenDTO = new AgenDTO();
@@ -57,6 +61,7 @@ public class AgenController {
      * @author: YuHangChen
      * @time: 09/08/2020 上午10:17
      */
+    @ApiOperation(value = "按单位编码删除单位")
     @PostMapping("/remove")
     public String remove(@RequestBody AgenVO fabAgenVO){
         AgenDTO fabAgenDTO = new AgenDTO();
@@ -75,6 +80,7 @@ public class AgenController {
      * @author: YuHangChen
      * @time: 09/08/2020 上午10:17
      */
+    @ApiOperation(value = "用于修改单位信息")
     @PostMapping("/update")
     public String update(@RequestBody AgenVO fabAgenVO){
         AgenDTO fabAgenDTO = new AgenDTO();
@@ -93,6 +99,7 @@ public class AgenController {
      * @author: YuHangChen
      * @time: 09/08/2020 上午10:17
      */
+    @ApiOperation(value = "根据单位编码查询单位")
     @PostMapping("/getByAgenCode")
     public String getByAgenCode(@RequestBody AgenVO fabAgenVO){
         AgenDTO fabAgenDTO = new AgenDTO();
@@ -111,6 +118,7 @@ public class AgenController {
      * @author: YuHangChen
      * @time: 09/08/2020 上午10:17
      */
+    @ApiOperation(value = "根据单位名查询单位")
     @PostMapping("/getByAgenName")
     public String getByAgenName(@RequestBody AgenVO fabAgenVO){
         AgenDTO fabAgenDTO = new AgenDTO();
@@ -128,6 +136,7 @@ public class AgenController {
      * @author: YuHangChen
      * @time: 09/08/2020 上午10:17
      */
+    @ApiOperation(value = "用于查看单位列表")
     @GetMapping("/listAll")
     public String listAll(){
         List<AgenDTO> fabAgenDTOList = fabAgenService.listAll();
@@ -141,6 +150,7 @@ public class AgenController {
      * @param pageVO
      * @return
      */
+    @ApiOperation(value = "通过分页查询")
     @PostMapping("listByPage")
     public String listByPage(@RequestBody PageVO pageVO) {
         PageDTO pageDTO = MyBeanUtil.copyProperties(pageVO, PageDTO.class);
@@ -156,6 +166,7 @@ public class AgenController {
      * @param fabAgenVOList
      * @return
      */
+    @ApiOperation(value = "批量删除单位")
     @PostMapping("removeBatch")
     public String removeBatch(@RequestBody List<AgenVO> fabAgenVOList) {
         List<AgenDTO> fabAgenDTOList = MyBeanUtil.copyListProperties(fabAgenVOList, AgenDTO.class);
@@ -172,6 +183,7 @@ public class AgenController {
      * @author: YuHangChen
      * @time: 09/08/2020 上午10:17
      */
+    @ApiOperation(value = "根据部门编码查询单位")
     @PostMapping("/getByDeptCode")
     public String getByDeptCode(@RequestBody AgenVO fabAgenVO){
         AgenDTO fabAgenDTO = new AgenDTO();
