6bb96a606cde63d3d02225e7c77a887ddbf769f7
UoweMe <rootupup@163.com>
2020-08-13 08:50:57 +0800

    异常信息以及部分业务逻辑待处理

--- ecds-bill-accounting/src/main/java/com/bosssoft/ecds/enums/CbillAccountingCode.java
+++ ecds-bill-accounting/src/main/java/com/bosssoft/ecds/enums/CbillAccountingCode.java
@@ -9,10 +9,25 @@ import com.bosssoft.ecds.common.response.ResultCode;
  * @Date 2020/8/12 11:39
  * @Version 1.0
  */
-public class CbillAccountingCode implements ResultCode {
+public enum CbillAccountingCode implements ResultCode {
 
     //自定义错误类型及返回码
-
+    //自定义错误类型及返回码
+    SUCCESS(true, 1000, "操作成功！"),
+    //操作失败时返回的状态码
+    INSERT_FAIL(false,1001,"插入失败"),
+    UPDATE_FAIL(false,1002,"更新失败"),
+    DELETE_FAIL(false,1003,"删除失败"),
+    BILL_SERIAL_ID_NOT_EXIST(false, 1004, "该票据校验码不存在"),
+    PAYER_TEL_NOT_EXIST(false,1005,"该缴款人电话不存在"),
+    ACCOUNT_ILLEGAL(false, 1006, "该票据校验码对应入账金额不正确"),
+    ACCOUNT_TYPE_ILLEGAL(false,1007,"该入账类型不存在"),
+    ACCOUNT_TIME_ILLEGAL(false,1008,"入账时间不合理"),
+    BILL_NO_NOT_EXIST(false, 1009, "票据号码不存在"),
+    BILL_NO_ILLEGAL(false, 1010, "票据号码不合理"),
+    BILL_BATCH_ID_NOT_EXIST(false, 1011, "票据批次号不存在"),
+    TIME_ILLEGAL(false, 1012, "开票时间必须在入账时间之后"),
+    ;
 
     boolean success;
 
