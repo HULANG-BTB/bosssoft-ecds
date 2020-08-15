a29572ed20b8f393aa09277a3fd030f8638822d7
wy <3478949971@qq.com>
2020-08-15 22:09:14 +0800

    增删改查

--- ecds-base/src/main/java/com/bosssoft/ecds/controller/SubjectController.java
+++ ecds-base/src/main/java/com/bosssoft/ecds/controller/SubjectController.java
@@ -1,14 +1,11 @@
 package com.bosssoft.ecds.controller;
 
-
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.bosssoft.ecds.common.response.CommonCode;
 import com.bosssoft.ecds.common.response.QueryResponseResult;
 import com.bosssoft.ecds.common.response.ResponseResult;
-import com.bosssoft.ecds.dao.SubjectDao;
 import com.bosssoft.ecds.entity.dto.SubjectDTO;
-import com.bosssoft.ecds.entity.po.SubjectPO;
+import com.bosssoft.ecds.entity.vo.subjectvo.SubjectQueryVO;
 import com.bosssoft.ecds.entity.vo.subjectvo.SubjectVO;
+import com.bosssoft.ecds.entity.vo.subjectvo.UpdateSubjectVO;
 import com.bosssoft.ecds.service.SubjectService;
 import com.bosssoft.ecds.utils.MyBeanUtil;
 import io.swagger.annotations.Api;
@@ -16,9 +13,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * <p>
@@ -36,23 +30,30 @@ public class SubjectController {
     @Autowired
     SubjectService subjectService;
 
-    @ApiOperation(value = "根据3级科目id返回其及其下4级科目")
-    @GetMapping("/getFourthByPID/{pid}")
-    public QueryResponseResult getFourthByPID(@PathVariable long pid) {
-        List<SubjectDTO> subjectDTOS = subjectService.getFourthByPID(pid);
-        List<SubjectVO> subjectVOS = MyBeanUtil.copyListProperties(subjectDTOS, SubjectVO::new);
-        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS, subjectVOS);
-        return queryResponseResult;
-
+    @ApiOperation(value = "分页条件查询接口")
+    @PostMapping("/listPage")
+    public QueryResponseResult listPage(@RequestBody @Validated SubjectQueryVO subjectQueryVO) {
+        return subjectService.listPage(subjectQueryVO);
     }
 
-    @ApiOperation(value = "添加预算科目",notes = "类别名称和编码唯一")
+    @ApiOperation(value = "添加预算科目",notes = "每年的类别名称和编码唯一")
     @PostMapping("/add")
     public ResponseResult add(@RequestBody @Validated SubjectVO subjectVO){
         SubjectDTO subjectDTO = MyBeanUtil.copyProperties(subjectVO, SubjectDTO.class);
-        subjectService.add(subjectDTO);
-        return new ResponseResult(CommonCode.SUCCESS);
+        return subjectService.add(subjectDTO);
+    }
 
+
+    @ApiOperation(value = "修改预算科目")
+    @PostMapping("/update")
+    public QueryResponseResult update(@RequestBody @Validated UpdateSubjectVO updateSubjectVO){
+        return subjectService.update(updateSubjectVO);
+    }
+
+    @ApiOperation(value = "删除预算科目")
+    @PostMapping("/delete")
+    public QueryResponseResult update(Long id){
+        return subjectService.delete(id);
     }
 }
 
--- ecds-base/src/main/java/com/bosssoft/ecds/entity/vo/subjectvo/UpdateSubjectVO.java
+++ ecds-base/src/main/java/com/bosssoft/ecds/entity/vo/subjectvo/UpdateSubjectVO.java
@@ -2,6 +2,7 @@ package com.bosssoft.ecds.entity.vo.subjectvo;
 
 import com.baomidou.mybatisplus.extension.activerecord.Model;
 import com.bosssoft.ecds.constant.CheckConstant;
+import com.bosssoft.ecds.constant.IncomeSortConstant;
 import com.bosssoft.ecds.constant.SubjectConstant;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -9,48 +10,29 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 import org.springframework.beans.factory.annotation.Value;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.util.List;
 
 /**
- * @author wy
- * @create 2020-08-10 20:20
+ * @author shkstart
+ * @create 2020-08-14 22:24
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@ApiModel(value="SubjectVO对象", description="")
-public class SubjectVO extends Model<SubjectVO> {
+@ApiModel(value="UpdateSubjectVO对象", description="")
+public class UpdateSubjectVO extends Model<UpdateSubjectVO> {
 
+    @NotNull(message = SubjectConstant.Subject_ID_NOT_NULL)
     @ApiModelProperty(value = "科目id")
     private Long id;
-
-    @NotBlank(message = SubjectConstant.Subject_YEAR_NOT_NULL)
-    @ApiModelProperty(value = "年度")
-    private String year;
-
-    @NotBlank(message = SubjectConstant.Subject_CODE_NOT_NULL)
-    @Pattern(regexp = CheckConstant.INTEGER_REGULAR, message = SubjectConstant.CODE_IS_INTEGER)
-    @ApiModelProperty(value = "科目编码")
-    private String code;
 
     @NotBlank(message = SubjectConstant.Subject_NAME_NOT_NULL)
     @Pattern(regexp = CheckConstant.CHINESE_REGULAR, message = SubjectConstant.NAME_IS_CHINESE)
     @ApiModelProperty(value = "科目名称")
     private String name;
-
-    @ApiModelProperty(value = "层级")
-    private Integer level;
-
-    @Value("false")
-    @ApiModelProperty(value = "是否底级,0否，1是")
-    private Boolean leaf;
-
-    @NotNull(message = SubjectConstant.Subject_PID_NOT_NULL)
-    @ApiModelProperty(value = "父级ID")
-    private Long parentId;
 
     @Value("true")
     @ApiModelProperty(value = "是否启用，0否，1是")
@@ -59,6 +41,7 @@ public class SubjectVO extends Model<SubjectVO> {
     @ApiModelProperty(value = "备注")
     private String remark;
 
-    @ApiModelProperty(value = "下一级科目集合")
-    private List<SubjectVO> subjectVOS;
+    @Value("false")
+    @ApiModelProperty(value = "是否底级,0否，1是")
+    private Boolean leaf;
 }
