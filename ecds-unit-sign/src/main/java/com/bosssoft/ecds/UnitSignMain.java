39476448000a8e5b18d9852aa07b5f48c317acef
BigTomato <490721223@qq.com>
2020-08-16 18:15:37 +0800

    8-16:授权服务器与印章完成

--- ecds-unit-sign/src/main/java/com/bosssoft/ecds/UnitSignMain.java
+++ ecds-unit-sign/src/main/java/com/bosssoft/ecds/UnitSignMain.java
@@ -2,7 +2,7 @@ package com.bosssoft.ecds;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 /**
  * @author LiDaShan
