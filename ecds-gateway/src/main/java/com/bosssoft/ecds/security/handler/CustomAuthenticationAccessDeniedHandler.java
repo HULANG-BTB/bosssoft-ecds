3db546566a42e1825f547a8ee3926b2c13e89ce0
AloneH <admin@oibit.cn>
2020-08-17 18:13:02 +0800

    修改数据返回 不使用统一响应

--- ecds-gateway/src/main/java/com/bosssoft/ecds/security/handler/CustomAuthenticationAccessDeniedHandler.java
+++ ecds-gateway/src/main/java/com/bosssoft/ecds/security/handler/CustomAuthenticationAccessDeniedHandler.java
@@ -32,7 +32,7 @@ public class CustomAuthenticationAccessDeniedHandler implements ServerAccessDeni
         response.setStatusCode(HttpStatus.UNAUTHORIZED);
         response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
         // 三设置 验证异常返回信息
-        QueryResponseResult<String> responseResult = new QueryResponseResult<>(CommonCode.addEnum("授权失败", false, 40003, "Access Denied！"), denied.getMessage());
+        QueryResponseResult<String> responseResult = new QueryResponseResult<>(CommonCode.ACCESSDENIED, denied.getMessage());
         byte[] responseBodyBytes = JSONUtil.toJsonStr(responseResult).getBytes();
         // 写入信息
         return response.writeWith(Mono.just(response.bufferFactory().wrap(responseBodyBytes)));
