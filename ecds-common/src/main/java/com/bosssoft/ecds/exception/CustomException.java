a8ff516428f37beed52da7f324161f92b29db7f5
lsir <15188366128@163.com>
2020-08-17 10:10:13 +0800

    在公共模块添加异常捕获相关代码，并在pom文件添加com.google.guava依赖

---
+++ ecds-common/src/main/java/com/bosssoft/ecds/exception/CustomException.java
@@ -0,0 +1,26 @@
+package com.bosssoft.ecds.exception;
+
+
+import com.bosssoft.ecds.response.ResultCode;
+
+/**
+ * 自定义异常，用于在抛出异常时携带统一响应数据
+ */
+public class CustomException extends RuntimeException {
+
+    private ResultCode resultCode;
+
+    public CustomException(ResultCode resultCode){
+        this.resultCode = resultCode;
+    }
+
+    public ResultCode getResultCode() {
+        return resultCode;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = resultCode != null ? resultCode.hashCode() : 0;
+        return 31*result;
+    }
+}
