f32362bc978e6e7a565102307282dda3ed91e571
lsir <15188366128@163.com>
2020-08-18 17:18:18 +0800

    添加sentinel相关代码

--- ecds-gateway/src/main/java/com/bosssoft/ecds/config/MyBlockRequestHandler.java
+++ ecds-gateway/src/main/java/com/bosssoft/ecds/config/MyBlockRequestHandler.java
@@ -2,9 +2,11 @@ package com.bosssoft.ecds.config;
 
 import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
 import com.alibaba.fastjson.JSON;
-import com.bosssoft.ecds.code.ExceptionDetail;
 import com.bosssoft.ecds.code.GatewayCode;
+import com.bosssoft.ecds.exception.ExceptionDetail;
+import com.bosssoft.ecds.response.ResponseResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -13,6 +15,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -26,12 +29,12 @@ public class MyBlockRequestHandler implements BlockRequestHandler {
 
     @Override
     public Mono<ServerResponse> handleRequest(ServerWebExchange exchange, Throwable t) {
-        String massage = "Blocked by Sentinel: " + t.getClass().getSimpleName() + ",time:"+new Date();
+        String massage = "Blocked by Sentinel: " + t.getClass().getSimpleName() + ",time:"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         String tags = t.getClass().getSimpleName();
         ExceptionDetail detail = new ExceptionDetail(GatewayCode.TOO_MANY_REQUESTS,massage,tags);
         log.info(JSON.toJSONString(detail));
         return ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .body(BodyInserters.fromValue(GatewayCode.TOO_MANY_REQUESTS));
+                .body(BodyInserters.fromValue(new ResponseResult(GatewayCode.TOO_MANY_REQUESTS)));
     }
 }
