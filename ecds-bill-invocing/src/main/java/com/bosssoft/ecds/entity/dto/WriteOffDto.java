15c2a6dd567b5a5d66131b8f1b4791f94bf6ba02
Dododoo <752093374@qq.com>
2020-08-19 17:15:58 +0800

    提交

---
+++ ecds-bill-invocing/src/main/java/com/bosssoft/ecds/entity/dto/WriteOffDto.java
@@ -0,0 +1,33 @@
+package com.bosssoft.ecds.entity.dto;
+
+import com.bosssoft.ecds.entity.po.UneCbillItem;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class WriteOffDto {
+    /**
+     * 票据编码
+     */
+    private String fBillId;
+
+    /**
+     * 票据批次号
+     */
+    private String fBillBatchCode;
+
+    /**
+     * 票据号码
+     */
+    private String fBillNo;
+
+    /**
+     * 开票金额
+     */
+    private double fTotalAmt;
+
+    /***
+     * 项目明细
+     */
+    private List<UneCbillItem> uneCbillItems;
+}
