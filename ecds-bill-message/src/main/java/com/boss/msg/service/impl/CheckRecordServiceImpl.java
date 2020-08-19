cfde83ed01bc7d97d0f1b203776905e828264ad9
dkhaos <e@qq.com>
2020-08-19 16:50:07 +0800

    解决服务调用不到的bug

---
+++ ecds-bill-message/src/main/java/com/boss/msg/service/impl/CheckRecordServiceImpl.java
@@ -0,0 +1,15 @@
+package com.boss.msg.service.impl;
+
+import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.boss.msg.entity.po.CheckRecordPo;
+import com.boss.msg.mapper.CheckRecordMapper;
+import com.boss.msg.service.CheckRecordService;
+/**
+ * @author zhangxiaohui
+ * @create 2020/8/14 11:34
+ */
+@Service
+public class CheckRecordServiceImpl extends ServiceImpl<CheckRecordMapper, CheckRecordPo> implements CheckRecordService{
+
+}
---
+++ ecds-bill-message/src/main/java/com/boss/msg/task/MailTask.java
@@ -0,0 +1,48 @@
+package com.boss.msg.task;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.boss.msg.entity.po.MailPo;
+import com.boss.msg.mapper.MailMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Date;
+
+/**
+ * @author zhangxiaohui
+ * @create 2020/8/13 15:01
+ */
+@Slf4j
+@Component
+public class MailTask {
+
+    @Resource
+    private MailMapper mailMapper;
+
+    /**
+     * 定时删除数据库中发件时间超过30天的邮件
+     */
+    @Async
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void delMailTask() {
+        QueryWrapper<MailPo> query = getQuery();
+        mailMapper.delete(query);
+        log.info(Thread.currentThread().getName() + "邮件定时任务定时任务");
+    }
+
+    /**
+     * 获取邮件发送时间在30天前的查询对象
+     *
+     * @return QueryWrapper
+     */
+    public QueryWrapper<MailPo> getQuery() {
+        QueryWrapper<MailPo> query = new QueryWrapper<>();
+        // 获取30天前的Date对象
+        Date date = new Date(System.currentTimeMillis() - 30 * 24 * 60 * 60 * 1000L);
+        query.le("f_mail_sentDate", date);
+        return query;
+    }
+}
