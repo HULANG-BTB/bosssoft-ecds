e42840f841e76a958260f524df5d7a5159eb81a1
wuliming <18120805709@163.com>
2020-08-20 14:10:48 +0800

    移动类到子包中

--- ecds-base/src/main/java/com/bosssoft/ecds/entity/vo/itembillvo/ItemBillVO.java
+++ ecds-base/src/main/java/com/bosssoft/ecds/entity/vo/itembillvo/ItemBillVO.java
@@ -1,4 +1,4 @@
-package com.bosssoft.ecds.entity.vo;
+package com.bosssoft.ecds.entity.vo.itembillvo;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
