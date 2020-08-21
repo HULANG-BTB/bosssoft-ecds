3c1d78448c3731c0c714243674c89d0e50b2da00
AloneH <admin@oibit.cn>
2020-08-21 23:41:07 +0800

    修复鉴权一致返回false

--- ecds-gateway/src/main/java/com/bosssoft/ecds/security/auth/CustomReactiveAuthorizationManager.java
+++ ecds-gateway/src/main/java/com/bosssoft/ecds/security/auth/CustomReactiveAuthorizationManager.java
@@ -47,7 +47,7 @@ public class CustomReactiveAuthorizationManager implements ReactiveAuthorization
                     String reqMethod = permissionVO.getMethod();
                     // 如果 A 具有访问 /user 的权限，那么 A自动具有访问 /user/* 的权限 此逻辑可以修改 地址和访问方式同时确定是否具有权限
                     if (reqUrl != null && reqUrl.startsWith(this.path) && method != null && reqMethod.equalsIgnoreCase(this.method)) {
-                        return false;
+                        return true;
                     }
                     return false;
                 })
