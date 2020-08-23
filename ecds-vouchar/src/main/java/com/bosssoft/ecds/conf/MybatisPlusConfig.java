2e09ddb67a8b182aa8ecf76cc16a0226332bd717
ke <liuke311@126.com>
2020-08-23 17:19:20 +0800

    修改配置文件.

--- ecds-vouchar/src/main/java/com/bosssoft/ecds/conf/MybatisPlusConfig.java
+++ ecds-vouchar/src/main/java/com/bosssoft/ecds/conf/MybatisPlusConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
 import java.util.Date;
 
 /**
@@ -34,14 +35,14 @@ public class MybatisPlusConfig {
             @Override
             public void insertFill(MetaObject metaObject) {
 
-                /*String authId = httpServletRequest.getHeader("auth_id");
-                String authNickname = httpServletRequest.getHeader("auth_nickname");*/
+                String authId = httpServletRequest.getHeader("auth_id");
+                String authNickname = new String(Base64.getDecoder().decode(httpServletRequest.getHeader("auth_nickname")));
                 /**
                  * 生产环境使用上面的方式获取ID和nickname
                  * 测试环境模拟获得ID和nickname使用下面的方式
                  */
-                Long authId = 1L;
-                String authNickname = "test";
+                /*Long authId = 1L;
+                String authNickname = "test";*/
                 this.setFieldValByName("createTime", new Date(), metaObject);
                 this.setFieldValByName("updateTime", new Date(), metaObject);
                 this.setFieldValByName("operatorId", authId, metaObject);
@@ -52,15 +53,14 @@ public class MybatisPlusConfig {
             @Override
             public void updateFill(MetaObject metaObject) {
 
-                /*String authId = httpServletRequest.getHeader("auth_id");
-                String authNickname = httpServletRequest.getHeader("auth_nickname");*/
+                String authId = httpServletRequest.getHeader("auth_id");
+                String authNickname = new String(Base64.getDecoder().decode(httpServletRequest.getHeader("auth_nickname")));
                 /**
                  * * 生产环境使用上面的方式获取ID和nickname
                  * * 测试环境模拟获得ID和nickname使用下面的方式
                  */
-
-                Long authId = 1L;
-                String authNickname = "test";
+                /*Long authId = 1L;
+                String authNickname = "test";*/
                 this.setFieldValByName("updateTime", new Date(), metaObject);
                 this.setFieldValByName("operatorId", authId, metaObject);
                 this.setFieldValByName("operator", authNickname, metaObject);
