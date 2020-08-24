0ff25c0b43489c2bfb6499fca4a9ab2797cc8800
lve <734843455@qq.com>
2020-08-24 09:51:04 +0800

    删除文件 ecds-bill-write-off/src/main

--- ecds-bill-write-off/src/main/java/com/bosssoft/ecds/dao/MonitorRecordMapper.java
+++
@@ -1,7 +0,0 @@
-package com.bosssoft.ecds.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.bosssoft.ecds.entity.po.MonitorRecordPO;
-
-public interface MonitorRecordMapper extends BaseMapper<MonitorRecordPO> {
-}
--- ecds-bill-write-off/src/main/java/com/bosssoft/ecds/service/UnitWriteOffService.java
+++
@@ -1,8 +0,0 @@
-package com.bosssoft.ecds.service;
-
-/**
- * @author hujierong
- * @date 2020-8-12
- */
-public interface UnitWriteOffService {
-}
